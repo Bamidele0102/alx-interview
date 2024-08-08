277591cfe1fffa2f1924bdb1a61ae56bb6386ffa
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-08-08 21:28:06 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,24 @@
 #!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie.
- * The first positional argument passed is the Movie ID
- * Display one character name per line in the same order
- * as listed in the /films/ endpoint
- */
+const request = require('request');
 
-const request = require('request-promise-native');
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.alx-tools.com/api/films/';
+const endpoint = 'https://swapi-api.hbtn.io/api';
+const filmId = process.argv[2];
 
-async function fetchCharacters() {
-  try {
-    // Makes API request to get the film data
-    const body = await request(filmURL + filmNum);
-    
-    // Parse the response to get the list of character URLs
-    const charURLList = JSON.parse(body).characters;
+request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
+  if (error) return console.log(error);
 
-    // Iterate over each character URL and fetch their data
-    for (const charURL of charURLList) {
-      const charBody = await request(charURL);
-      const character = JSON.parse(charBody);
-      console.log(character.name);
-    }
-  } catch (err) {
-    console.error(err);
+  const characters = JSON.parse(body).characters;
+
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve(body);
+        }
+      });
+    });
   }
-}
-
-fetchCharacters();
+});
